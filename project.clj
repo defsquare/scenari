635694dbfcc84514f1f1e394b5afd65fd295c827
jeremiegrodziski <jeremie.github@grodziski.com>
2014-08-15 17:01:20 +0200

    add a licence and define a correct url for the project

--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
 (defproject spexec "1.0.0"
               :description "A BDD - Behavior Driven Development - library for Clojure"
-              :url "http://github.com/jgrodziski/spexec"
+              :url "http://github.com/zenmodeler/spexec"
               :dependencies [[org.clojure/clojure "1.6.0"]
                              [instaparse "1.3.3"]
                              [com.taoensso/timbre "3.2.1"]]
+              :license {:name "MIT License"
+                        :url "http://opensource.org/licenses/MIT"
+                        :distribution :repo}
               :source-paths ["src" "src/main/clojure"])
 
