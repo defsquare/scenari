589a32d5402cd7584d2c1f1d22047794d9310975
jeremiegrodziski <jeremie.github@grodziski.com>
2019-10-01 11:30:10 +0200

    Bump to version 1.4.5 and spit/render related metadata in file(s).

--- src/scenari/meta.clj
+++ src/scenari/meta.clj
@@ -3,6 +3,6 @@
 
 (def module-name "scenari")
 (def path ".")
-(def version "1.4.4")
-(def tag "v1.4.4")
-(def generated-at "2019-09-18T11:47:56Z")
+(def version "1.4.5")
+(def tag "v1.4.5")
+(def generated-at "2019-10-01T09:30:10Z")
