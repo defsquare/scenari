7eccd9895099d4637e7a7db71c2a12df1f51a4a9
davidpanza <david.panza@gmail.com>
2025-08-26 12:21:22 +0200

    Bump to version 2.0.9-alpha and manually modify related metadata in file(s)

--- src/scenari/meta.clj
+++ src/scenari/meta.clj
@@ -3,6 +3,6 @@
 
 (def module-name "scenari")
 (def path ".")
-(def version "2.0.8-alpha")
-(def tag "v2.0.8-alpha")
-(def generated-at "2025-08-26T12:00:00Z")
+(def version "2.0.9-alpha")
+(def tag "v2.0.9-alpha")
+(def generated-at "2025-08-26T13:00:00Z")
